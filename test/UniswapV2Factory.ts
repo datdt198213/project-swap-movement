af34c554fee9d21e77aaf89c9bc8d36803f54c80
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2024-10-02 17:26:06 +0700

    add sample test suit of UniswapV2Factory contract

---
+++ test/UniswapV2Factory.ts
@@ -0,0 +1,23 @@
+import hre from 'hardhat'
+import {expect} from 'chai'
+import { UniswapV2Factory__factory } from '../typechain-types';
+import { Contract, Signer } from 'ethers';
+
+describe("UniswapV2Factory", function () {
+  var uniswapV2FactoryContract: UniswapV2Factory__factory;
+  var uniswapV2Factory: Contract;
+  var deployer : Signer;
+  
+  this.beforeEach(async function() {
+      [deployer] = await hre.ethers.getSigners();
+      uniswapV2FactoryContract = await hre.ethers.getContractFactory("UniswapV2Factory", deployer)
+      uniswapV2Factory = await uniswapV2FactoryContract.deploy(await deployer.getAddress());
+  })
+
+  describe("setFeeTo(address)", async function () {
+      it('Set feeTo successfully!', async function () {
+          await expect(uniswapV2Factory.setFeeTo(await deployer.getAddress())).not.reverted
+      })
+  });
+  
+});
