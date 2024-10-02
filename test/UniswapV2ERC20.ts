75fc81b28cc29daee77a51fa11840e3bdd8a97bf
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2024-10-02 17:12:43 +0700

    add sample test suit of UniswapV2ERC20 contract

--- test/UniswapV2ERC20.ts
+++ test/UniswapV2ERC20.ts
@@ -1,9 +1,23 @@
-import { expect } from "chai";
+import {expect} from "chai";
 import { ethers, Contract, Signer } from "ethers";
 import hre from "hardhat";
 import { UniswapV2ERC20__factory } from "../typechain-types";
+import { describe } from "mocha";
 
-describe("UniswapV2ERC20", async function () {
-    var uniswapV2ERC20: UniswapV2ERC20__factory;
+describe("UniswapV2ERC20", function () {
+    var uniswapV2ERC20Contract: UniswapV2ERC20__factory;
+    var uniswapV2ERC20: Contract;
+    var deployer : Signer;
+    this.beforeEach(async function() {
+        [deployer] = await hre.ethers.getSigners();
+        uniswapV2ERC20Contract = await hre.ethers.getContractFactory("UniswapV2ERC20", deployer)
+        uniswapV2ERC20 = await uniswapV2ERC20Contract.deploy();
+    })
+
+    describe("approve(address, uint)", async function () {
+        it('Approve successfully!', async function () {
+            await expect(uniswapV2ERC20.approve(await deployer.getAddress(), 10)).not.reverted
+        })
+    });
     
-})
+});
