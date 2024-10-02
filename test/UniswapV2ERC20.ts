8111b3fabc6bf9e7f76211335e83c91527a8b190
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2024-10-02 14:20:24 +0700

    Add test folder for testing contracts

---
+++ test/UniswapV2ERC20.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import { ethers, Contract, Signer } from "ethers";
+import hre from "hardhat";
+import { UniswapV2ERC20__factory } from "../typechain-types";
+
+describe("UniswapV2ERC20", async function () {
+    var uniswapV2ERC20: UniswapV2ERC20__factory;
+    
+})
